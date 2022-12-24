6e482e248079de8d159428f9f749fdd4f0572005
Andrey <andrejsatykov@yandex.ru>
2022-12-24 23:24:59 +0300

    Natural numbers from N to 1.

---
+++ Task_001/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+Console.Clear();
+Console.WriteLine("Показать натуральные числа от N до 1.");
+int EnterNumber(string message) // Метод ввода данных 
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());    
+}
+int endingNum = 1;
+int initialVal = EnterNumber("Введите число N: "); 
+FinalResult(initialVal, endingNum);
+void FinalResult(int initialVal, int endingNum)
+{
+    if(initialVal >= endingNum)
+    {
+        Console.Write(initialVal + "; ");
+        initialVal--;
+        FinalResult(initialVal, endingNum);
+    }
+}
+
+
