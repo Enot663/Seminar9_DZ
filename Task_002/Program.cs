6200f86244af5f1627f10136a51d38c09a5d63c9
Andrey <andrejsatykov@yandex.ru>
2022-12-24 23:59:19 +0300

    The sum of natural numbers from M to N

---
+++ Task_002/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear();
+Console.WriteLine("Сумма натуральных чисел от M до N.");
+int EnterNumber(string message) // Метод ввода данных 
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());    
+}
+int naturalNumbersM = EnterNumber("Введите число M: ");
+int naturalNumbersN = EnterNumber("Введите число N: ");
+
+int SumNumbers(int naturalNumbersM, int naturalNumbersN)
+{
+    if(naturalNumbersN == naturalNumbersM) return naturalNumbersN;
+    return naturalNumbersM + SumNumbers(naturalNumbersM + 1, naturalNumbersN);
+}
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {naturalNumbersM} до {naturalNumbersN} равна {SumNumbers(naturalNumbersM, naturalNumbersN)}");
